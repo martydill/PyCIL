111c05f91f269fad44681cbe9919aa0290997d5e
martydill <marty@code-ninja.org>
2010-09-26 20:59:26 -0700

    Added test file for multiplication

---
+++ tests/mult.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        // returns 26
+        static int Main(string[] args)
+        {
+
+            int a = 1;
+            a *= 13;
+            a *= 2;
+            return a;
+        }
+    }
+}
