bbae4b0f4264ea0b42f99a6c4fe2214f8e7fe51f
martydill <marty@code-ninja.org>
2010-10-17 10:24:15 -0700

    Fixed parsing of classes with classes as fields

--- src/Parser/ClassParser.py
+++ src/Parser/ClassParser.py
@@ -26,8 +26,14 @@ class ClassParser(object):
                 v = Variable()
                 visibility = parserContext.get_next_token()
                 type = parserContext.get_next_token() # fixme - type, visibility
+                if type == 'class':
+                    type = parserContext.get_next_token()
+                
                 if Types.BuiltInTypes.has_key(type):
                     v.type = Types.BuiltInTypes[type]
+                else:
+                    v.type = Types.resolve_type(type)
+                    
                 name = parserContext.get_next_token()
                
                 v.name = name
@@ -109,7 +115,6 @@ class ClassParserTests(unittest.TestCase):
         c = cp.parse(p)
         
         self.assertEqual(Types.resolve_type('ConsoleApplication1.bar').classRef, c)
-
    
     def test_method_in_class_gets_class_namespace(self):
         from ParserContext import ParserContext
@@ -134,3 +139,33 @@ class ClassParserTests(unittest.TestCase):
         
         self.assertEqual(c.methods[0].name, 'ctor')
         self.assertEqual(c.methods[0].namespace, 'ConsoleApplication1.foo' )
+
+    def test_parse_class_with_class_field(self):
+        from ParserContext import ParserContext
+        s = '''
+            .class private auto ansi beforefieldinit ConsoleApplication1.bar
+                   extends [mscorlib]System.Object
+            {
+              .field public class ConsoleApplication1.foo f
+              .method public hidebysig specialname rtspecialname 
+                      instance void  .ctor() cil managed
+              {
+                // Code size       7 (0x7)
+                .maxstack  8
+                IL_0000:  ldarg.0
+                IL_0001:  call       instance void [mscorlib]System.Object::.ctor()
+                IL_0006:  ret
+              } // end of method bar::.ctor
+            
+            } // end of class ConsoleApplication1.bar'''
+
+        p = ParserContext(s)
+        cp = ClassParser()
+        c = cp.parse(p)
+        
+        self.assertEqual(c.methods[0].name, 'ctor')
+        self.assertEqual(c.methods[0].namespace, 'ConsoleApplication1.bar' )
+        self.assertEquals(len(c.fieldDefinitions), 1)
+        f = c.fieldDefinitions[0]
+        self.assertEqual(f.name, 'f')
+        self.assertEqual(f.type, Types.resolve_type('ConsoleApplication1.foo'))
